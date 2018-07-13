81ea4dcc25538e5db4add757f26cd9b3f8d01368
julius75 <juliusnchagwa75@gmail.com>
2018-07-13 03:03:20 +0300

    new suggestion changes

--- app/src/main/java/com/chain/finalapp/DashBoardActivity.java
+++ app/src/main/java/com/chain/finalapp/DashBoardActivity.java
@@ -1,13 +1,29 @@
 package com.chain.finalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
 
 public class DashBoardActivity extends AppCompatActivity {
+    private CardView suggest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        suggest = findViewById(R.id.suggestion);
+        suggest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashBoardActivity.this,SuggestionActivity.class);
+                startActivity(intent);
+                Toast.makeText(DashBoardActivity.this, "suggest", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
