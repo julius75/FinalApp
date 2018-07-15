ca3fb28b4afffe8663df80000b71aba2d1c0e7c5
julius75 <juliusnchagwa75@gmail.com>
2018-07-15 15:59:23 +0300

    login activity

--- app/src/main/java/com/chain/finalapp/LoginActivity.java
+++ app/src/main/java/com/chain/finalapp/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.chain.finalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.lgBtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
