fd9aa26cc576f437821c27d1621440d756c8da80
julius75 <juliusnchagwa75@gmail.com>
2018-07-15 17:18:48 +0300

    new

--- app/src/main/java/com/chain/finalapp/LoginPage.java
+++ app/src/main/java/com/chain/finalapp/LoginPage.java
@@ -1,13 +1,26 @@
 package com.chain.finalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
+    private Button dash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        dash = findViewById(R.id.dash);
+        dash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this,DashBoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
